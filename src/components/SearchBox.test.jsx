1d9d9ed2a6a6f22f5a5f1a43fa3ebbaeffaa70ea
Yasindu Janapriya <yasindurivipiyum@gmail.com>
2026-01-04 10:24:12 +0530

    Set up the Test enviroment

---
+++ src/components/SearchBox.test.jsx
@@ -0,0 +1,4 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SearchBox from './SearchBox';
